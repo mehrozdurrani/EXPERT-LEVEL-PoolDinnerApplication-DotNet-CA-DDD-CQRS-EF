e3cf0425569ae7bb7c9a5a544872ab2bf6442f44
mehrozdurrani <mehrozdurrani@gmail.com>
2023-09-01 11:27:35 +0200

    Code Analysis and format fixation in Test codes

--- tests/UnitTests/PoolDinner.Application.UnitTests/Menus/Commands/TestUtils/CreateMenuCommandUtils.cs
+++ tests/UnitTests/PoolDinner.Application.UnitTests/Menus/Commands/TestUtils/CreateMenuCommandUtils.cs
@@ -5,24 +5,31 @@ namespace PoolDinner.Application.UnitTests.Menus.Commands.TestUtils
 {
     public static class CreateMenuCommandUtils
     {
-        public static CreateMenuCommand CreateCommand(List<CreateMenuSectionCommand>? sections = null) =>
-        new CreateMenuCommand(
+        public static CreateMenuCommand CreateCommand(List<CreateMenuSectionCommand>? sections = null)
+        {
+            return new(
             Constants.Host.Id.Value,
             Constants.MenuConstants.Name,
             Constants.MenuConstants.Description,
-            sections ?? CreateSectionsCommand(2)
-        );
+            sections ?? CreateSectionsCommand(2));
+        }
+
         public static List<CreateMenuSectionCommand> CreateSectionsCommand(int sectionCount = 1,
         List<CreateMenuItemsCommand>? items = null)
-        => Enumerable.Range(0, sectionCount).Select(index => new CreateMenuSectionCommand(
-            Name: $"Menu Section Name {index}",
-            Description: $"Menu Section Description {index}",
-            Items: items ?? CreateItemsCommand(1)
-        )).ToList();
+        {
+            return Enumerable.Range(0, sectionCount).Select(index => new CreateMenuSectionCommand(
+                    Name: $"Menu Section Name {index}",
+                    Description: $"Menu Section Description {index}",
+                    Items: items ?? CreateItemsCommand(1)
+                )).ToList();
+        }
+
         public static List<CreateMenuItemsCommand> CreateItemsCommand(int itemCount)
-        => Enumerable.Range(0, itemCount).Select(index => new CreateMenuItemsCommand(
-            Name: $"Menu Item Name {index}",
-            Description: $"Menu Item Description{index}"
-        )).ToList();
+        {
+            return Enumerable.Range(0, itemCount).Select(index => new CreateMenuItemsCommand(
+                    Name: $"Menu Item Name {index}",
+                    Description: $"Menu Item Description{index}"
+                )).ToList();
+        }
     }
 }
