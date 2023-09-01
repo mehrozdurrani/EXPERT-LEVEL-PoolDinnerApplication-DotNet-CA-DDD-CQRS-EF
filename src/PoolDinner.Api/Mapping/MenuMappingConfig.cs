b250125bb504cc22ddcf2979838ba8948c1ca7fc
mehrozdurrani <mehrozdurrani@gmail.com>
2023-09-01 16:26:40 +0200

    Error fixation in MenuMapConfig. All Ids were converted to Guid but Response was string string

--- src/PoolDinner.Api/Mapping/MenuMappingConfig.cs
+++ src/PoolDinner.Api/Mapping/MenuMappingConfig.cs
@@ -15,20 +15,20 @@ namespace PoolDinner.Api.Mapping
                 Map(dest => dest, src => src.Request);
 
             config.NewConfig<Menu, MenuResponse>().
-                Map(dest => dest.Id, src => src.Id.Value).
+                Map(dest => dest.Id, src => src.Id.Value.ToString()).
                 Map(dest => dest.AverageRating, src => src.AverageRating.Value).
-                Map(dest => dest.HostId, src => src.HostId.Value).
-                Map(dest => dest.DinnerIds, src => src.DinnerIds.Select(dinnerId => dinnerId.Value)).
-                Map(dest => dest.MenuReviewIds, src => src.MenuReviewIds.Select(menuReviewId => menuReviewId.Value)).
+                Map(dest => dest.HostId, src => src.HostId.Value.ToString()).
+                Map(dest => dest.DinnerIds, src => src.DinnerIds.Select(dinnerId => dinnerId.Value.ToString())).
+                Map(dest => dest.MenuReviewIds, src => src.MenuReviewIds.Select(menuReviewId => menuReviewId.Value.ToString())).
                 Map(dest => dest.Sections, src => src.Sections);
 
             config.NewConfig<MenuSection, MenuSectionResponse>().
-                Map(dest => dest.Id, src => src.Id.Value).
+                Map(dest => dest.Id, src => src.Id.Value.ToString()).
                 Map(dest => dest.Items, src => src.Items);
 
             config.NewConfig<MenuItem, MenuItemResponse>().
                 Map(dest => dest.Id,
-                    src => src.Id.Value);
+                    src => src.Id.Value.ToString());
         }
     }
 }
--- src/PoolDinner.Infrastructure/Migrations/20230901093231_InitialCreateV1.Designer.cs
+++ src/PoolDinner.Infrastructure/Migrations/20230901093231_InitialCreateV1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PoolDinner.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using PoolDinner.Infrastructure.Persistence;
 namespace PoolDinner.Infrastructure.Migrations
 {
     [DbContext(typeof(PoolDinnerDbContext))]
-    partial class PoolDinnerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230901093231_InitialCreateV1")]
+    partial class InitialCreateV1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
