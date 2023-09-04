1ef64972c10dbe2886dbc4b7f1e121f99e2d2551
mehrozdurrani <mehrozdurrani@gmail.com>
2023-09-04 16:00:05 +0200

    Code Refactor/Formatting of project pooldinner.infrastructure

--- src/PoolDinner.Infrastructure/DependencyInjection.cs
+++ src/PoolDinner.Infrastructure/DependencyInjection.cs
@@ -15,42 +15,60 @@ namespace PoolDinner.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            ConfigurationManager configuration
+        )
         {
             services.AddAuth(configuration);
             services.AddPersistance(configuration);
             return services;
         }
 
-        public static IServiceCollection AddPersistance(this IServiceCollection services, ConfigurationManager configurationManager)
+        public static IServiceCollection AddPersistance(
+            this IServiceCollection services,
+            ConfigurationManager configurationManager
+        )
         {
-            string connectionString = configurationManager.GetConnectionString("PoolDinnerDbConnection")!;
-            services.AddDbContext<PoolDinnerDbContext>(options => options.UseSqlServer(connectionString));
+            string connectionString = configurationManager.GetConnectionString(
+                "PoolDinnerDbConnection"
+            )!;
+            services.AddDbContext<PoolDinnerDbContext>(
+                options => options.UseSqlServer(connectionString)
+            );
             services.AddScoped<DbContext, PoolDinnerDbContext>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IMenuRepository, MenuRepository>();
             return services;
         }
 
-        public static IServiceCollection AddAuth(this IServiceCollection services, ConfigurationManager configuration)
+        public static IServiceCollection AddAuth(
+            this IServiceCollection services,
+            ConfigurationManager configuration
+        )
         {
             var jwtSettings = new JwtSettings();
             configuration.Bind(JwtSettings.SectionName, jwtSettings);
             services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
             services.AddSingleton(Options.Create(jwtSettings));
             services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
-            services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme).
-                AddJwtBearer(options =>
-                options.TokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = jwtSettings.Issuer,
-                    ValidAudience = jwtSettings.Audience,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
-                });
+            services
+                .AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(
+                    options =>
+                        options.TokenValidationParameters = new TokenValidationParameters()
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidIssuer = jwtSettings.Issuer,
+                            ValidAudience = jwtSettings.Audience,
+                            IssuerSigningKey = new SymmetricSecurityKey(
+                                Encoding.UTF8.GetBytes(jwtSettings.Secret)
+                            )
+                        }
+                );
             return services;
         }
     }
